eacf5ac24f999986e803077887aee612f7b084cb
Javed Ali <javedali.bcs18@iba-suk.edu.pk>
2023-03-21 14:11:21 +0500

    Added some important modules for railway

--- server.js
+++ server.js
@@ -20,9 +20,21 @@ const Middlewares = require('./Middlewares/Middlewares')
 const app = express();
 DB();
 
+// Set up rate limiter: maximum of twenty requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+
+
 
 
 // Middlewares
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // parse res.body
 app.use(express.json())
